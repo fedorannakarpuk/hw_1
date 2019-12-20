269b9e468cb1c0ee537b6c9701604b4f86f36b21
Anna <akarpuk007@gmail.com>
2019-12-20 19:06:06 +0300

    Ознакомление завершено. Конечный продукт.

--- functions_for_working_with_arrays.php
+++ functions_for_working_with_arrays.php
@@ -1,36 +1,115 @@
-<li>     : <br>
+<li>  usort - Сортирует массив по значениям используя пользовательскую функцию для сравнения элементов   : <br>
     <?php
+    function cmp($a, $b){
+        if ($a == $b) {
+            return 0;
+        }
+        return ($a < $b) ? -1 : 1;
+    }
 
+    $a = array(3, 2, 5, 4, 1);
+
+    usort($a, "cmp");
+    echo "<pre>";
+    var_dump($a);
+    echo "</pre>";
     ?>  </li>
-<li>     : <br>
+<li>   is_array — Определяет, является ли переменная массивом  : <br>
     <?php
-
+    $yes = array('это', 'массив');
+    echo is_array($yes) ? 'Массив' : 'Не массив';
+    echo "<br>";
+    $no = 'это строка';
+    echo is_array($no) ? 'Массив' : 'Не массив';
     ?>  </li>
-<li>     : <br>
+<li>  implode — Объединяет элементы массива в строку : <br>
     <?php
-
+    $array = array('имя', 'почта', 'телефон');
+    $comma_separated = implode(" + ", $array);
+    echo $comma_separated; // имя,почта,телефон
     ?>  </li>
-<li>     : <br>
+<li> array_fill — Заполняет массив значениями : <br>
     <?php
-
+    $a = array_fill(5, 6, 'banana');
+    $b = array_fill(-2, 4, 'pear');
+    echo "<pre>";
+    print_r($a);
+    echo "</pre>";
+    echo "<pre>";
+    print_r($b);
+    echo "</pre>";
     ?>  </li>
-<li>     : <br>
+<li> array_filter — Фильтрует элементы массива с помощью callback-функции  : <br>
     <?php
+    function odd($var){
+        // является ли переданное число нечетным
+        return($var & 1);
+    }
 
+    function even($var){
+        // является ли переданное число четным
+        return(!($var & 1));
+    }
+
+    $array1 = array("a"=>1, "b"=>2, "c"=>3, "d"=>4, "e"=>5);
+    $array2 = array(6, 7, 8, 9, 10, 11, 12);
+    echo "Нечетные: ";
+    print_r(array_filter($array1, "odd"));
+    echo "<br>";
+    echo "Четные: ";
+    print_r(array_filter($array2, "even"));
     ?>  </li>
-<li>     : <br>
+<li> count — Подсчитывает количество элементов массива или что-то в объекте  : <br>
     <?php
-
+    $a[0] = 1;
+    $a[1] = 3;
+    $a[2] = 5;
+    $b =[5,3,1];  // а так считает не сумму значений, а кол-во эл-тов массива
+    $result = count($a);
+    $result1 = count($b);
+    echo $result." и ".$result1;
     ?>  </li>
-<li>     : <br>
+<li>  sort — Сортирует массив и rsort — Сортирует массив в обратном порядке   : <br>
     <?php
+    $fruits = array("lemon", "orange", "banana", "apple");
+    echo "<pre>";
+    var_dump(sort($fruits));  // возвращает булевское значение
+    echo "</pre>";
 
+    foreach ($fruits as $key => $val) {
+        echo "fruits[" . $key . "] = " . $val . "<br>";
+    }
+    echo '<br>';
+
+    rsort($fruits);   // возвращает булевское значение
+    foreach ($fruits as $key => $val) {
+        echo "$key = $val<br>";
+    }
     ?>  </li>
-<li>     : <br>
+<li> range — Создает массив, содержащий диапазон элементов, shuffle — Перемешивает массив  : <br>
     <?php
-
+    $numbers = range(1, 20);
+    shuffle($numbers);
+    foreach ($numbers as $number) {
+        echo $number." , ";
+    }
     ?>  </li>
-<li>     : <br>
+<li>  array_slice — Выбирает срез массива, array_slice() возвращает последовательность элементов массива array, определённую параметрами offset и length.     : <br>
     <?php
+    $input = array("a", "b", "c", "d", "e");
 
-    ?>  </li>
+    $output1 = array_slice($input, 2);      //  возвращает "c", "d" и "e"
+    $output2 = array_slice($input, -2, 1);  // возвращает "d"
+    $output3 = array_slice($input, 0, 3);   // возвращает "a", "b" и "c"
+    echo "<pre>";
+    print_r($output1);
+    echo "</pre>";
+    echo "<pre>";
+    print_r($output2);
+    echo "</pre>";
+    echo "<pre>";
+    print_r($output3);
+    echo "</pre>";
+
+    ?>
+</li>
--- index.php
+++ index.php
@@ -13,51 +13,57 @@ include "data_type.php";
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
+<a name="top">  </a>
+<div class="wrapper">
+
+    <p> 1) Типы данных 	&#9989; </p>
+    <?php echo $a;?>
+
+    <p> 2) Переменные 	&#9989; </p>
+    <?php echo $b;?>
+
+    <p> 3) Константы. Магические константы 	&#9989; </p>
+    <?php echo $c." ".$d;?>
+
+    <p> 4) Стандарт кодирования 	&#9989; </p>
+    <?php echo $e;?>
+
+    <p> 5) Онлайн PHP редактор 	&#9989; </p>
+    <img src="1.png" alt="" width=100%">
+    <br>
+
+    <p> 6) Строковые функции 	&#9989; </p>
+    <span>Ознакомилась со следующими функциями: </span>
+    <ul>
+        <?php  include "string_function.php"; ?>
+    </ul>
+    <?php echo $f;?>
+
+    <p> 7) Математические функции 	&#9989; </p>
+    <ul>
+        <?php  include "math_function.php"; ?>
+    </ul>
+    <?php echo $f;?>
 
 
-<p> 1) Типы данных 	&#9989; </p>
-<?php echo $a;?>
-
-<p> 2) Переменные 	&#9989; </p>
-<?php echo $b;?>
-
-<p> 3) Константы. Магические константы 	&#9989; </p>
-<?php echo $c." ".$d;?>
-
-<p> 4) Стандарт кодирования 	&#9989; </p>
-<?php echo $e;?>
-
-<p> 5) Онлайн PHP редактор 	&#9989; </p>
-<img src="1.png" alt="" width=100%">
-<br>
-
-<p> 6) Строковые функции 	&#9989; </p>
-<span>Ознакомилась со следующими функциями: </span>
-<ol>
-    <?php  include "string_function.php"; ?>
-</ol>
-<?php echo $f;?>
-
-<p> 7) Математические функции 	&#9989; </p>
-<ol>
-    <?php  include "math_function.php"; ?>
-</ol>
-<?php echo $f;?>
+    <p> 8) Список всех подезных функций 	&#9989; </p>
+    <a href="http://php.net/manual/ru/funcref.php"> По ссылке</a>
+    <ul>
+        <?php  include "function_reference.php"; ?>
+    </ul>
+    <?php echo $g;?>
 
 
-<p> 8) Список всех подезных функций 	&#9989; </p>
-<a href="http://php.net/manual/ru/funcref.php"> По ссылке</a>
-<ol>
-    <?php  include "function_reference.php"; ?>
-</ol>
-<?php echo $g;?>
+    <p> 9) Функции работы с массивами 	&#9989; </p>
+    <?php  include "functions_for_working_with_arrays.php";
+    echo $i;?>
 
 
+    <br><br><br>
+    <a href="#top" class="top"> &uArr;&uArr;&uArr;  <br> НАВЕРХ </a>
+    <br><br>
 
-<p> 9) Функции работы с массивами 	&#9989; </p>
-<?php  include "functions_for_working_with_arrays.php";
-echo $i;?>
-
+</div>
 
 
 </body>
